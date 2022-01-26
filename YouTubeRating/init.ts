c41e0b0e8c6fdab9394a4ea7883ee01f96911397
Matthew Beaudreault <bougie517@gmail.com>
2022-01-26 10:54:59 -0500

    refactoring

--- YouTubeRating/init.ts
+++ YouTubeRating/init.ts
@@ -1,6 +1,38 @@
+function fnAddButtons(value, id) {
+  var btn = document.createElement("input");
+  btn.value = value;
+  btn.id = id;
+  btn.type = "submit";
+  document.querySelector("div[id='top-level-buttons-computed']").appendChild(btn);
+}
 
+function fnDefineEvents(rating) {
+  document
+    .getElementById("like-btn")
+    .addEventListener("click", function (event) {
+      chrome.runtime.sendMessage({msg: "user-liked"}, (response) => {
+        if (!response) {
+          console.log("not recieved");
+        }else {
+          console.log("received");
+        }
+      })
+    });
 
-(function () {
+    document
+      .getElementById("dislike-btn")
+      .addEventListener("click", function (event) {
+        chrome.runtime.sendMessage({msg: "user-disliked"}, (response) => {
+          if (response) {
+            console.log("received");
+          } else {
+            console.log("not received ")
+          }
+        })
+      });
+}
+
+function main() {
   var jsInitChecktimer = setInterval(checkForJS_Finish, 111);
 
   function checkForJS_Finish () {
@@ -11,37 +43,6 @@
       fnDefineEvents();
     }
   }
-  function fnAddButtons(value, id) {
-    var btn = document.createElement("input");
-    btn.value = value;
-    btn.id = id;
-    btn.type = "submit";
-    document.querySelector("div[id='top-level-buttons-computed']").appendChild(btn);
-  }
+}
 
-  function fnDefineEvents(rating) {
-    document
-      .getElementById("like-btn")
-      .addEventListener("click", function (event) {
-        chrome.runtime.sendMessage({msg: "user-liked"}, (response) => {
-          if (!response) {
-            console.log("not recieved");
-          }else {
-            console.log("received");
-          }
-        })
-      });
-
-      document
-        .getElementById("dislike-btn")
-        .addEventListener("click", function (event) {
-          chrome.runtime.sendMessage({msg: "user-disliked"}, (response) => {
-            if (response) {
-              console.log("received");
-            } else {
-              console.log("not received ")
-            }
-          })
-        });
-  }
-})();
+main();
