27c5f9fe03837638a5359e2f20cb96a7ebea7db3
Matthew Beaudreault <bougie517@gmail.com>
2022-02-04 15:23:38 -0500

    Adding UI flag elements and communication between buttons and messger

--- src/init.ts
+++ src/init.ts
@@ -1,47 +1,73 @@
-function fnAddButtons(doc, value, id) {
+
+
+function fnAddButtons(doc, value, id, inputLocation) {
   var btn = doc.createElement("input");
   btn.value = value;
   btn.id = id;
   btn.type = "submit";
-  doc.querySelector("div[id='top-level-buttons-computed']").appendChild(btn);
+  doc.querySelector(inputLocation).appendChild(btn);
 }
 
-function fnDefineEvents(doc, chromeVar) {
-  console.log(doc.buttons);
+function addTextNode(doc, text, inputLocation) {
+  const newDiv = doc.createElement("div");
+
+  const newTextNode = doc.createTextNode(text);
+  newDiv.appendChild(newTextNode);
+  doc.querySelector(inputLocation).appendChild(newTextNode);
+}
+
+function fnDefineEvents(id, msg, doc, chromeVar) {
   doc
-    .getElementById("like-btn")
+    .getElementById(id)
     .addEventListener("click", function (event) {
-      chromeVar.runtime.sendMessage({msg: "user-liked"}, (response) => {
+      chromeVar.runtime.sendMessage({msg: msg}, (response) => {
         if (!response) {
           console.log("not recieved");
         }else {
-          console.log("received");
+          console.log(response.data);
         }
       })
     });
+}
 
-    doc
-      .getElementById("dislike-btn")
-      .addEventListener("click", function (event) {
-        chromeVar.runtime.sendMessage({msg: "user-disliked"}, (response) => {
-          if (response) {
-            console.log("received");
-          } else {
-            console.log("not received ")
-          }
-        })
-      });
+function getData(chromeVar, msg) {
+  return new Promise((resolve, reject) => {
+    chromeVar.runtime.sendMessage({msg: msg}, (response) => {
+      if (response) {
+        resolve(response.data);
+      } else {
+        reject("no message response");
+      }
+    });
+  })
 }
 
 function main(doc, chromeVar) {
   var jsInitChecktimer = setInterval(checkForJS_Finish, 111);
 
-  function checkForJS_Finish () {
+  async function checkForJS_Finish () {
     if (doc.querySelector("div[id='top-level-buttons-computed']")) {
       clearInterval(jsInitChecktimer);
-      fnAddButtons(doc, "like", "like-btn");
-      fnAddButtons(doc, "dislike", "dislike-btn");
-      fnDefineEvents(doc, chromeVar);
+      await getData(chromeVar, "add-numLikes");
+      const numLikes = await getData(chromeVar, 'get-numLikes');
+      fnAddButtons(doc, numLikes + " like", "like-btn", "div[id='top-level-buttons-computed']");
+      fnDefineEvents("like-btn", "add-numLikes", doc, chromeVar);
+      const numDislikes = await getData(chromeVar, 'get-numDislikes');
+      fnAddButtons(doc, numDislikes + " dislike", "dislike-btn", "div[id='top-level-buttons-computed']");
+      fnDefineEvents("dislike-btn", "add-numDislikes", doc, chromeVar);
+      const numMisinformation = await getData(chromeVar, 'get-numMisinformation');
+      fnAddButtons(doc, numMisinformation + " Misinformation", "Misinformation-flag-btn", "div[id='info-contents']");
+      const numDidNotWork = await getData(chromeVar, 'get-numDidNotWork');
+      fnAddButtons(doc, numDidNotWork + " Didn't Work", "Didn't-work-flag-btn", "div[id='info-contents']");
+      const numOutdated = await getData(chromeVar, 'get-numOutdated');
+      fnAddButtons(doc, numOutdated + " Outdated", "Outdated-flag-btn", "div[id='info-contents']");
+      const numOffensive = await getData(chromeVar, 'get-numOffensive');
+      fnAddButtons(doc, numOffensive + " Offensive", "Offensive-flag-btn", "div[id='info-contents']");
+      const numImmoral = await getData(chromeVar, 'get-numImmoral');
+      fnAddButtons(doc, numImmoral + " Immoral", "Immoral-flag-btn", "div[id='info-contents']");
+
+      addTextNode(doc, numLikes + " Likes ", "div[id='info-contents']");
+      addTextNode(doc, numDislikes + " Disikes", "div[id='info-contents']");
     }
   }
 }
@@ -52,5 +78,3 @@ try{
 } catch(e) {
   console.log(e);
 }
-
-export { fnDefineEvents, fnAddButtons };
--- src/messenger.js
+++ src/messenger.js
@@ -1,9 +1,31 @@
+const ratingNumMap = new Map();
+ratingNumMap.set('numLikes', 0);
+ratingNumMap.set('numDislikes', 0);
+ratingNumMap.set('numMisinformation', 0);
+ratingNumMap.set('numDidNotWork', 0);
+ratingNumMap.set('numOutdated', 0);
+ratingNumMap.set('numOffensive', 0);
+ratingNumMap.set('numImmoral', 0);
+
+
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request) {
-    console.log("msg received");
-    if (request.msg == "user-liked") {
-      console.log("msg received");
+    if (request.msg.split("-", 1)[0] == "add") {
+      addToData(request.msg.split("-", 2)[1]);
       sendResponse({ sender: "messenger.ts", data: "received"});
+    } else if (request.msg.split("-", 1)[0] == "get") {
+      numData = getData(request.msg.split("-", 2)[1]);
+      sendResponse({sender: "messenger.ts", data: numData});
     }
   }
 });
+
+
+function addToData(dataName) {
+  ratingNumMap.set(dataName, ratingNumMap.get(dataName) + 1);
+}
+
+function getData(dataName) {
+  return ratingNumMap.get(dataName);
+}
